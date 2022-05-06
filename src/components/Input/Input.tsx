0bb30aa567b4270e347bc6b1312e54b4a3b2eac9
Nolan Eckert <nolan.eckert@gmail.com>
2022-05-06 14:00:39 -0600

    Abstract styling to separate file

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,38 +1,18 @@
 import { FC } from "react"
-import styled from "styled-components"
 
 import { InputProps } from "./Input.types"
+import {
+  StyledInput,
+  StyledLabel,
+  StyledMessage,
+  StyledText
+} from "./input-styles"
 
-const StyledInput = styled.input<InputProps>`
-  height: 40px;
-  width: 300px;
-  border-radius: 3px;
-  border: solid 2px ${props => props.disabled ? "#e4e3ea" : (props.error ? "#a9150b" : (props.success ? "#067d68" : "#353637"))};
-  background-color: #fff;
-
-  &:focus {
-    border: solid 2px #1b116e;
-  }
-`
-
-const StyledLabel = styled.div<InputProps>`
-  font-size: 14px;
-  color: ${props => props.disabled ? "#e4e3ea" : "#080808"};
-  padding-bottom: 6px;
-`
-
-const StyledMessage = styled.div<InputProps>`
-  font-size: 14px;
-  color: #a9150b;
-  padding-top: 4px;
-`
-
-const StyledText = styled.p<InputProps>`
-  margin: 0;
-  color: ${props => props.disabled ? "#e4e3ea" : (props.error ? "#a9150b" : "#080808")};
-`
-
-const Input: FC<InputProps> = ({ disabled, error, id, label, message, onChange, placeholder, success, ...props }) => {
+const Input: FC<InputProps> = ({
+  disabled, error, id,
+  label, message, onChange,
+  placeholder, success, ...props
+}) => {
   return (
     <>
       <StyledLabel>
